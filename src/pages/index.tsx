1e2d05ccb74ad4ef04b35680fd5af21af1f24fa8
saurabhan <saurabhan.001@gmail.com>
2022-10-02 21:19:05 +0530

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import Sidebar from '@/components/sidebar';
 import { Meta } from '@/layouts/Meta';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: Unreachable code error
+// eslint-disable-next-line
+BigInt.prototype.toJSON = function (): number {
+  return Number(this);
+};
+
 const Index = () => {
   return (
     <>
